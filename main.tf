c8c287b668eecac63f0d60d3367ca77541ba2ffc
Nivall <38981440+k-kotov@users.noreply.github.com>
2021-11-16 10:08:45 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,4 @@
-
-resource "null_resource" "test" {
- 
-}
-
-output "test" {
-  value =  file("output.json")
-}
+resource "null_resource" "resource1" {
+  provisioner "local-exec" {
+    command = "python3 your_file.py"
+  }
